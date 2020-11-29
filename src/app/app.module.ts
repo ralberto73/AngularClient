f8813ba35e1301f3b54d6c230f09a2aaf5a6a762
Raul Perez <ralberto73@gmail.com>
2020-11-29 14:45:50 -0500

    Firs Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WorkOrderListComponent } from './work-order-list/work-order-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorkOrderListComponent
   ],
   imports: [
     BrowserModule,
